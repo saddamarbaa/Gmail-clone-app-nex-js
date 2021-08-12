7b7225faa7b5add1fe503356208b37b81dd62fd0
saddamarbaa <saddamarbaasaddamarbaa@gmail.com>
2021-08-12 14:09:19 +0700

    add: loading spinner

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,33 @@
 
 import Head from "next/head";
 import "../styles/globals.css";
+import Router from "next/router";
 
 import { store } from "../app/store";
 import { Provider } from "react-redux";
+
+import ProgressBar from "@badrap/bar-of-progress";
+
+const progress = new ProgressBar({
+	// The size (height) of the progress bar.
+	// Numeric values get converted to px.
+	size: 2,
+
+	// Color of the progress bar.
+	// Also used for the glow around the bar.
+	color: "#29e",
+
+	// Class name used for the progress bar element.
+	className: "bar-of-progress",
+
+	// How many milliseconds to wait before the progress bar
+	// animation starts after calling .start().
+	delay: 80,
+});
+
+Router.events.on("routeChangeStart", progress.start);
+Router.events.on("routeChangeComplete", progress.finish);
+Router.events.on("routeChangeError", progress.finish);
 
 function MyApp({ Component, pageProps }) {
 	return (
